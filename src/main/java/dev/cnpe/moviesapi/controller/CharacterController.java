465332e481aab4637cecc1e8897936d2e0a880b4
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-10-28 12:26:03 -0300

    :sparkles: feat: add endpoints for search and get1

--- src/main/java/dev/cnpe/moviesapi/controller/CharacterController.java
+++ src/main/java/dev/cnpe/moviesapi/controller/CharacterController.java
@@ -1,23 +1,40 @@
 package dev.cnpe.moviesapi.controller;
 
+import dev.cnpe.moviesapi.model.dto.CharacterResponse;
+import dev.cnpe.moviesapi.model.dto.CharacterSearchCriteria;
 import dev.cnpe.moviesapi.model.dto.CharacterSearchResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.cnpe.moviesapi.model.service.CharacterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/characters")
+@RequiredArgsConstructor
 public class CharacterController {
 
+    private final CharacterService characterService;
 
-    @GetMapping
-    public List<CharacterSearchResult> getAllCharacters(){
-        return null;
+    @PostMapping
+    public List<CharacterSearchResult> advancedSearch(@RequestBody CharacterSearchCriteria searchCriteria) {
+        return characterService.search(searchCriteria);
     }
 
 
+    @GetMapping
+    public List<CharacterSearchResult> searchCharacters(@RequestParam(required = false) String name,
+                                                        @RequestParam(required = false) Integer age,
+                                                        @RequestParam(required = false) Double weight,
+                                                        @RequestParam(required = false) String movieTitle) {
 
+        return characterService.search(new CharacterSearchCriteria(name, age, weight, movieTitle));
+    }
+
+
+    @GetMapping("/{id}")
+    public CharacterResponse getCharacterById(@PathVariable(name = "id") Long id) {
+        return characterService.getCharacterById(id);
+    }
 
 }
