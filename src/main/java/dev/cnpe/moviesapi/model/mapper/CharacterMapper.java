62e6c6b009140cf2404b373881e36622618033af
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-10-28 17:38:29 -0300

    :hammer: chore: update mapper

--- src/main/java/dev/cnpe/moviesapi/model/mapper/CharacterMapper.java
+++ src/main/java/dev/cnpe/moviesapi/model/mapper/CharacterMapper.java
@@ -1,13 +1,27 @@
 package dev.cnpe.moviesapi.model.mapper;
 
+import dev.cnpe.moviesapi.model.dto.CharacterCreationRequest;
 import dev.cnpe.moviesapi.model.dto.CharacterResponse;
 import dev.cnpe.moviesapi.model.entity.Character;
+import dev.cnpe.moviesapi.model.entity.Movie;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Mapper(componentModel = "spring", uses = {MovieMapper.class})
 public interface CharacterMapper {
 
     @Mapping(target = "movies", source = "movies")
     CharacterResponse toResponse(Character character);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "movies", expression = "java(initMovies())")
+    Character toEntity(CharacterCreationRequest creationRequest);
+
+
+    default Set<Movie> initMovies() {
+        return new HashSet<>();
+    }
 }
