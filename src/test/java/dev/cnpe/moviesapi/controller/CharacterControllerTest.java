550ea55ac1b87d40f239674d8a2b6d4ac0336e5e
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-10-28 12:30:34 -0300

    :sparkles: feat: test init

--- src/test/java/dev/cnpe/moviesapi/controller/CharacterControllerTest.java
+++ src/test/java/dev/cnpe/moviesapi/controller/CharacterControllerTest.java
@@ -1,18 +1,22 @@
 package dev.cnpe.moviesapi.controller;
 
+import dev.cnpe.moviesapi.model.dto.CharacterSearchCriteria;
+import dev.cnpe.moviesapi.model.dto.CharacterSearchResult;
+import dev.cnpe.moviesapi.model.service.CharacterService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CharacterController.class)
@@ -21,13 +25,28 @@ class CharacterControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private CharacterService characterService;
+
     @Test
     @WithMockUser
-    void shouldReturnAllCharactersWhenRequested() throws Exception {
+    void shouldReturnListOfCharacters() throws Exception {
 
-        mockMvc.perform(get("/api/v1/characters")
-                       .contentType(MediaType.APPLICATION_JSON))
-               .andExpect(status().isOk());
+        given(characterService.search(any(CharacterSearchCriteria.class)))
+                .willReturn(List.of(
+                                new CharacterSearchResult(1L, "Movie 1", "https://url.to/image-1"),
+                                new CharacterSearchResult(2L, "Movie 2", "https://url.to/image-2"),
+                                new CharacterSearchResult(3L, "Movie 3", "https://url.to/image-3")
+                        )
+                );
 
+        mockMvc.perform(post("/api/v1/characters")
+                       .with(csrf())
+                       .contentType(MediaType.APPLICATION_JSON)
+                       .content("{}") //empty search criteria
+               )
+               .andExpect(status().isOk())
+               .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+               .andExpect(jsonPath("$.length()").value(3));
     }
 }
