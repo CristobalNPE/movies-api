dc0deaea808599e5e862ee6946fca006b950ed2b
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-10-29 15:48:58 -0300

    :hammer: chore: fix search and update method signatures

--- src/main/java/dev/cnpe/moviesapi/model/service/CharacterService.java
+++ src/main/java/dev/cnpe/moviesapi/model/service/CharacterService.java
@@ -2,18 +2,18 @@ package dev.cnpe.moviesapi.model.service;
 
 import dev.cnpe.moviesapi.model.dto.*;
 import dev.cnpe.moviesapi.model.entity.Character;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CharacterService {
 
-    List<CharacterSearchResult> search(CharacterSearchCriteria searchCriteria);
+    Page<CharacterSearchResult> search(Pageable pageable, CharacterSearchCriteria searchCriteria);
 
     CharacterResponse getCharacterById(Long id);
 
     Character createCharacter(CharacterCreationRequest creationRequest);
 
-    void updateCharacter(CharacterUpdateRequest updateRequest);
+    void updateCharacter(CharacterUpdateRequest updateRequest, Long id);
 
     void deleteCharacter(Long id);
 
